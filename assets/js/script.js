4ce4e200177bba2dad0c77565ecb3c7cbaef72fe
Lã Cao Vũ đẹp trai <90137657+Vudzokkk@users.noreply.github.com>
2022-02-13 22:13:32 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function checkValid() {
     var input = document.getElementById("nhapngaysinh");
-    if (input.value == "21/11/2003") {
+    if (input.value == "31/03/2006") {
         showImage('./assets/texts/tunganh.txt');
     } else if (input.value == "20/03/2006") {
         showImage("assets/texts/Kyanbucu");
